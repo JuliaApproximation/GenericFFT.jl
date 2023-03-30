a254d1829dcec4041b1e59b0771cf60ffce95fd8
Documenter.jl <documenter@juliadocs.github.io>
2023-03-30 17:20:07 +0000

    build based on 2e55016

---
+++ v0.1.3/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#GenericFFT.jl-Documentation","page":"Home","title":"GenericFFT.jl Documentation","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"GenericFFT.jl implements a Fast Fourier Transform for generic floating point number types.","category":"page"}]
+}
---
+++ versions.js
@@ -0,0 +1,6 @@
+var DOC_VERSIONS = [
+  "stable",
+  "v0.1",
+];
+var DOCUMENTER_NEWEST = "v0.1.3";
+var DOCUMENTER_STABLE = "stable";
