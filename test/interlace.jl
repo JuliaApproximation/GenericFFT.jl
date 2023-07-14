8e706cdcf7394e16e52d15d21558045d038ba8ee
Milan Klöwer <milankloewer@gmx.de>
2023-07-14 11:29:04 +0200

    GenericFFT. added for (de)interace_complex

--- test/interlace.jl
+++ test/interlace.jl
@@ -4,20 +4,20 @@
 
             # deinterlace(interlace)
             v = rand(Complex{F},n)
-            v_interlaced = interlace_complex(v)
-            v2 = deinterlace_complex(v_interlaced)
+            v_interlaced = GenericFFT.interlace_complex(v)
+            v2 = GenericFFT.deinterlace_complex(v_interlaced)
             @test v == v2
 
             # and interlace(deinterlace)
             v_interlaced = rand(F,2n)
-            v = deinterlace_complex(v_interlaced)
-            v_interlaced2 = interlace_complex(v)
+            v = GenericFFT.deinterlace_complex(v_interlaced)
+            v_interlaced2 = GenericFFT.interlace_complex(v)
             @test v_interlaced == v_interlaced2
 
             # odd interlaced vectors (shouldn't be produced and will ignore last element)
             v_interlaced = rand(F,2n+1)
-            v = deinterlace_complex(v_interlaced)   # [end] ignored here
-            v_interlaced2 = interlace_complex(v)
+            v = GenericFFT.deinterlace_complex(v_interlaced)   # [end] ignored here
+            v_interlaced2 = GenericFFT.interlace_complex(v)
             @test v_interlaced[1:end-1] == v_interlaced2
         end
     end
