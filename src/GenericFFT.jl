30806dbf4c3df0ff8e20905f28a3c6861c4e4b4d
Jishnu Bhattacharya <jishnub.github@gmail.com>
2023-11-01 19:39:18 +0530

    Don't import unused names

--- src/GenericFFT.jl
+++ src/GenericFFT.jl
@@ -7,12 +7,10 @@ using Reexport
 
 import Base: *
 
-import AbstractFFTs: Plan, ScaledPlan,
-                     fft, ifft, bfft, fft!, ifft!, bfft!, rfft, irfft, brfft,
+import AbstractFFTs: Plan,
                      plan_fft, plan_ifft, plan_bfft, plan_fft!, plan_ifft!,
                      plan_bfft!, plan_rfft, plan_irfft, plan_brfft,
-                     fftshift, ifftshift, rfft_output_size, brfft_output_size,
-                     plan_inv, normalization
+                     plan_inv
 
 import FFTW: dct, dct!, idct, idct!, plan_dct!, plan_idct!,
              plan_dct, plan_idct, fftwNumber
